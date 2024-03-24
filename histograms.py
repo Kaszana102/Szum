c57a116c7e4d87744fc4d584cc89f0742e927d2f
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2024-03-24 21:13:56 +0100

    other histogram

--- histograms.py
+++ histograms.py
@@ -4,24 +4,22 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-
 def directory_histogram(directory):
-    hist_0 = [[0]]*256
-    hist_1 = [[0]]*256
-    hist_2 = [[0]]*256
+    hist_0 = [[0]] * 256
+    hist_1 = [[0]] * 256
+    hist_2 = [[0]] * 256
     for filename in os.listdir(directory):
         # load image
-        img = cv2.imread(directory+'/'+filename)
-
+        img = cv2.imread(directory + '/' + filename)
 
         hist_0 += cv2.calcHist([img], [0], None, [256], [0, 256])
         hist_1 += cv2.calcHist([img], [1], None, [256], [0, 256])
         hist_2 += cv2.calcHist([img], [2], None, [256], [0, 256])
         print(filename)
     plt.figure()
-    hist_0/= max(hist_0)
-    hist_1/= max(hist_1)
-    hist_2/= max(hist_2)
+    hist_0 /= max(hist_0)
+    hist_1 /= max(hist_1)
+    hist_2 /= max(hist_2)
     plt.plot([val[0] for val in hist_0], color='b')
     plt.plot([val[0] for val in hist_1], color='g')
     plt.plot([val[0] for val in hist_2], color='r')
@@ -29,9 +27,4 @@ def directory_histogram(directory):
     plt.show()
 
 
-<<<<<<< Updated upstream
-
 directory_histogram('dataset_src/horses')
-=======
-directory_histogram('dataset/penguins')
->>>>>>> Stashed changes
